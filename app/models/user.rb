d6ed8e6aca5dece9d5b7afc21fa991b3a4a0983f
KrithikaV11 <krithi.ramya@gmail.com>
2014-06-06 19:19:37 +0530

    Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+   #model tst
    attr_accessible :username, :email, :password, :mobile
    validates :username, :presence => true, length: { minimum: 2 }
    validates :password, :presence => true
